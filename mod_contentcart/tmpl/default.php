10bf8e3e706f0f7a11c7c9a528fd5d326ce74a36
Vadim Kunitsyn <zikkuratvk@gmail.com>
2018-08-09 15:00:36 +0200

    Опция отключения стилей для модуля, все стили вынесены в общий файл.

--- mod_contentcart/tmpl/default.php
+++ mod_contentcart/tmpl/default.php
@@ -8,7 +8,12 @@
  * @license 	GNU General Public License version 2 or later; see	LICENSE.txt
  */
 
+
 defined('_JEXEC') or die;
+$doc = JFactory::getDocument();
+if ($params->get('enable_css', 1)) {
+	$doc->addStyleSheet(JUri::root().'plugins/content/contentcart/assets/css/jlcontentcart.css', array('version' => 'auto'));
+}
 $plugin = JPluginHelper::getPlugin('content','contentcart');
 $pluginParams = new JRegistry($plugin->params);
 $session = JFactory::getSession();
@@ -20,23 +25,24 @@ if($pluginParams->get('mymenuitem')){
 }
 if ($session->get('content_order')) {
 ?>
-<div class="content_cart" style="border:1px solid #CCC;padding:10px;"> 
-<div class="content_cart_info" style="display:none;"> 
-<?php $i = 0; $total=0; foreach($session->get('content_order') as $order_item){ 
-	$order_item['title'];/*Название товара*/
-	$order_item['link'];/*Урл товара*/
-	$order_item['price'];/*Цена товара, если есть*/
-	$order_item['count'];/*количество товара*/
- $i++; $total=$total+($order_item['price']*$order_item['count']);} ?>
-</div>
-<p class="count"><span><?php echo JText::_('CONTENTCART_PRODUCTS_COUNT')?>: </span><span><?php echo ' '.count($session->get('content_order')); ?> </span></p>
-<p class="total"><span><?php echo JText::_('CONTENTCART_PRODUCT_TOTAL')?>: </span><span><?php echo ' '.$total.' '.$pluginParams->get('currency'); ?> </span></p>
-<a style="margin:0 auto;display:block;" class="btn btn-success" title="" href="<?php echo $cart_url ?>"><?php echo JText::_('CONTENTCART_GO_TO_CART')?></a>
+<div class="content_cart jlcontentcart"> 
+	<div class="content_cart_info" style="display:none;"> 
+	<?php $i = 0; $total=0; foreach($session->get('content_order') as $order_item){ 
+		$order_item['title'];/*Название товара*/
+		$order_item['link'];/*Урл товара*/
+		$order_item['price'];/*Цена товара, если есть*/
+		$order_item['count'];/*количество товара*/
+		$i++; $total=$total+($order_item['price']*$order_item['count']);} 
+	?>
+	</div>
+	<p class="count"><span><?php echo JText::_('CONTENTCART_PRODUCTS_COUNT')?>: </span><span><?php echo ' '.count($session->get('content_order')); ?> </span></p>
+	<p class="total"><span><?php echo JText::_('CONTENTCART_PRODUCT_TOTAL')?>: </span><span><?php echo ' '.$total.' '.$pluginParams->get('currency'); ?> </span></p>
+	<a class="jlcc-button jlcc-success" title="" href="<?php echo $cart_url ?>"><?php echo JText::_('CONTENTCART_GO_TO_CART')?></a>
 </div>
 <?php } else { ?>
-<div class="content_cart" style="border:1px solid #CCC;padding:10px;"> 
-<p class="count"><span><?php echo JText::_('CONTENTCART_PRODUCTS_COUNT')?>: </span><span><?php echo ' 0 ' ?> </span></p>
-<p class="total"><span><?php echo JText::_('CONTENTCART_PRODUCT_TOTAL')?>: </span><span><?php echo ' 0 '.$pluginParams->get('currency'); ?> </span></p>
-<a style="margin:0 auto;display:block;" onclick="return false" class="btn btn-primary" title="" href="<?php echo $cart_url ?>"><?php echo JText::_('CONTENTCART_EMPTY_CART')?></a>
+<div class="content_cart jlcontentcart" > 
+	<p class="count"><span><?php echo JText::_('CONTENTCART_PRODUCTS_COUNT')?>: </span><span><?php echo ' 0 ' ?> </span></p>
+	<p class="total"><span><?php echo JText::_('CONTENTCART_PRODUCT_TOTAL')?>: </span><span><?php echo ' 0 '.$pluginParams->get('currency'); ?> </span></p>
+	<a onclick="return false" class="jlcc-button jlcc-primary" title="" href="<?php echo $cart_url ?>"><?php echo JText::_('CONTENTCART_EMPTY_CART')?></a>
 </div>
 <?php } ?>
